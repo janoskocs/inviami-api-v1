6ce06040b6e895c43762ab525fb4c7d10ae47211
Janos Kocs <janos.kocs@outlook.com>
2024-03-19 10:36:40 +0000

    test: install testing and write first test

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
-
 const PORT = process.env.PORT || 5050;
 
-app.get('/', (req, res) => {
-  res.send({isAlive: 'True'});
-});
-
+  
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.status(200).json({isAlive: true});
+});
+  
 module.exports = app;
