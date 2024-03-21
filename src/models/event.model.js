da0765f0a1c68ca9ba83ef58e97dcd58e6d6be6e
Janos Kocs <janos.kocs@outlook.com>
2024-03-21 16:28:53 +0000

    refactor: update event model attendee properties

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -2,101 +2,102 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const eventSchema = new Schema({
-  eventName: {
-    type: String,
-    required: true,
-  },
-  customer: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  paid: {
-    type: Boolean,
-    default: false
-  },
-  adminCode: {
-    type: Number,
-    required: true,
-    default: 0o0,
-  },
-  refunded: {
-    type: Boolean,
-    default: false,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  eventDateTime: {
-    type: Date,
-    required: true,
-  },
-  RSPVBy: {
-    type: Date,
-    required: false
-  },
-  attendees: [
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-      },
-      notes: {
-        type: String,
-        default: null,
-      },
-      isComing: {
-        type: Boolean,
-        default: false
-      },
-      arrivedToEvent: {
-        type: Boolean,
-        default: false
-      },
-      deleted: {
-        type: Boolean,
-        default: false
-      }
+const eventSchema = new Schema(
+  {
+    eventName: {
+      type: String,
+      required: true,
     },
-  ],
-  invitationTemplate: {
-    type: String,
-    required: true
+    customer: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    paid: {
+      type: Boolean,
+      default: false,
+    },
+    adminCode: {
+      type: Number,
+      required: true,
+      default: 0o0,
+    },
+    refunded: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    eventDateTime: {
+      type: Date,
+      required: true,
+    },
+    RSPVBy: {
+      type: Date,
+      required: false,
+    },
+    attendees: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        email: {
+          type: String,
+          required: true,
+        },
+        notes: {
+          type: String,
+          default: null,
+        },
+        isAttending: {
+          type: Boolean,
+          default: false,
+        },
+        arrivedToEvent: {
+          type: Boolean,
+          default: false,
+        },
+        deleted: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+    invitationTemplate: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    eventApproval: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedDate: {
+      type: Date,
+      default: null,
+    },
   },
-  link: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  eventApproval: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  deleted: {
-    type: Boolean,
-    default: false,
-  },
-  deletedDate: {
-    type: Date,
-    default: null,
-  },
-},
 
-{ timestamps: true }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('Event', eventSchema);
